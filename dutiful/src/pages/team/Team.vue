53c24f53ca31c77aa9821f62c5ea1c1de164d037
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 03:36:18 -0700

    team and project component

---
+++ dutiful/src/pages/team/Team.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data: () => ({
+    token: "",
+    team: {},
+  }),
+  created() {
+    this.token = this.$route.query.token.toString() ?? "";
+  },
+  methods: {
+    teamInfo() {},
+  },
+});
+</script>
