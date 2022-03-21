b523c82eb073a212b472fadf562519b50aee1e60
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:30:18 -0700

    add loading functions

--- dutiful/src/services/message.ts
+++ dutiful/src/services/message.ts
@@ -4,11 +4,16 @@ export const showMessage = (vue: Vue, message: string) => {
     if (message.trim().length > 0) {
         (vue.$root.$refs.snackbar as any).open(message);
     }
-    (vue.$root.$refs.loading as any).close();
+    closeLoading(vue)
 }
 
 export const loading = (vue: Vue) => {
+       
     (vue.$root.$refs.loading as any).open();
+}
+
+export const closeLoading = (vue: Vue) => {        
+    (vue.$root.$refs.loading as any).close();
 }
 
 export const openDialog = (vue: Vue) => {
@@ -27,10 +32,6 @@ export const closeLightBox = (vue: Vue) => {
     (vue.$root.$refs.lightBox as any).close()
 }
 
-export const openChannelInfo = (vue: Vue) => {
-    (vue.$root.$refs.channelInfo as any).open()
-}
-
-export const closeChannelInfo = (vue: Vue) => {
-    (vue.$root.$refs.channelInfo as any).close()
+export const openConfirm = (vue: Vue, data: any) => {
+    (vue.$root.$refs.confirmDialog as any).open(data)
 }
