de6255756e2e197afa1bc04ed5244adc3d5c6af2
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:30:02 -0700

    loading teams action

--- dutiful/src/pages/team/TeamList.vue
+++ dutiful/src/pages/team/TeamList.vue
@@ -6,10 +6,16 @@
       outlined
       clearable
       append-icon="mdi-magnify"
+      @input="search"
     ></v-text-field>
     <v-col>
       <v-row>
-        <team v-for="team in teams" :key="team.id" :team="team" />
+        <team
+          @delete="teamDeleted"
+          v-for="(team, i) in teams"
+          :key="i"
+          :team="team"
+        />
       </v-row>
       <v-fab-transition>
         <v-btn
@@ -28,7 +34,7 @@
     </v-col>
     <app-dialog :title="dialogOptions.title" :titleColor="dialogOptions.color">
       <template v-slot:body>
-        <component :is="component"></component>
+        <component @upserted="teamUpsert" :is="component"></component>
       </template>
     </app-dialog>
   </v-col>
@@ -36,37 +42,77 @@
 
 <script lang="ts">
 import AppDialog from "@/components/core/AppDialog.vue";
-import { openDialog } from "@/services/message";
+import {
+  closeDialog,
+  closeLoading,
+  loading,
+  openDialog,
+  showMessage,
+} from "@/services/message";
 import Vue from "vue";
 import Upsert from "@/components/team/Upsert.vue";
 import Team from "@/components/team/Team.vue";
+import { getTeams } from "@/api/apis/team";
 
 export default Vue.extend({
   components: { AppDialog, Team },
   data: () => ({
-    teams: [
-      {
-        id: "10",
-        name: "test",
-        bio: "testing team for dutiful application",
-        projectsCount: 5,
-        usersCount: 5,
-        token:'adlasdaksdhkhasd'
-      },
-    ],
+    allTeams: [],
+    teams: [],
     component: Upsert,
     dialogOptions: {
       title: "Create New Team",
       color: "primary",
     },
   }),
+  watch: {
+    allTeams() {
+      this.teams = this.allTeams;
+    },
+  },
   beforeMount() {
     this.loadTeams();
   },
   methods: {
-    loadTeams() {},
+    loadTeams() {
+      loading(this);
+      getTeams()
+        .then((res) => {
+          if (res.status) {
+            this.allTeams = res.result;
+          }
+          showMessage(this, res.title);
+          closeLoading(this);
+        })
+        .catch((e) => {
+          showMessage(this, e.message);
+          closeLoading(this);
+        });
+    },
     newTeam() {
       openDialog(this);
+    },
+    teamUpsert(e: any) {
+      if (e.status) {
+        this.allTeams.push(e.team as never);
+        closeDialog(this);
+      }
+    },
+    teamDeleted(e: any) {
+      if (e.status) {
+        const teamIdnex = this.allTeams.findIndex(
+          (t: any) => t.token == e.team.token
+        );
+        this.allTeams.splice(teamIdnex, 1);
+      }
+    },
+    search(e: string) {
+      e = e.toLocaleLowerCase().trim();
+      this.teams = this.allTeams.filter(
+        (t: any) =>
+          t.name.toLocaleLowerCase().includes(e) ||
+          t.bio.toLocaleLowerCase().includes(e)
+      );
     },
   },
 });
