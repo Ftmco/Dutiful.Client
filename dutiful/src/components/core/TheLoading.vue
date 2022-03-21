99106a776040592fdfca850383485a8adc8ab6d7
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 23:29:01 -0700

    Update TheLoading.vue

--- dutiful/src/components/core/TheLoading.vue
+++ dutiful/src/components/core/TheLoading.vue
@@ -1,33 +1,37 @@
 <template>
-    <v-dialog v-model="dialog" hide-overlay persistent width="300">
-        <v-card color="primary" dark>
-            <v-card-text>
-                Please Wait
-                <v-progress-linear indeterminate
-                                   color="white"
-                                   class="mb-0"></v-progress-linear>
-            </v-card-text>
-        </v-card>
-    </v-dialog>
+  <v-dialog v-model="dialog" hide-overlay persistent width="300">
+    <v-card color="primary" dark>
+      <v-card-text>
+        Please Wait
+        <v-progress-linear
+          indeterminate
+          color="white"
+          class="mb-0"
+        ></v-progress-linear>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
-<script>
-    import Vue from 'vue'
+<script lang="ts">
+import Vue from "vue";
 
-    export default Vue.extend({
-        data: () => ({
-            dialog: false,
-        }),
-        created() {
-            this.$root.$refs.loading = this;
-        },
-        methods: {
-            open() {
-                this.dialog = true;
-            },
-            close() {
-                this.dialog = false;
-            },
-        },
-    })
+export default Vue.extend({
+  data: () => ({
+    dialog: false,
+  }),
+  created() {
+    this.$root.$refs.loading = this;
+  },
+  methods: {
+    open() {
+      console.log('loading opend',this.dialog);
+      this.dialog = true;
+    },
+    close() {
+      console.log('loading closed',this.dialog);
+      this.dialog = false;
+    },
+  },
+});
 </script>
